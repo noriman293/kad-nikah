0b9461ac7f47fa7fea664c6bce6d1ebb3b5dbaf9
Nor Iman <noriman293@gmail.com>
2026-09-10 18:19:14 +0800

    update: tukar gambar gallery dari photo5-7 ke photo1.jpg

--- src/pages/WeddingCard.jsx
+++ src/pages/WeddingCard.jsx
@@ -24,9 +24,7 @@ const IMAGES = {
   floralFrame: 'https://media.base44.com/images/public/69d4a417a8942610f2bb96f1/07b6b6dd1_generated_bf066ee2.png',
   monogram: '/image/mono.png', 
   gallery: [
-    '/image/photo5.jpg',  
-    '/image/photo6.jpg',      
-    '/image/photo7.jpg',
+    '/image/photo1.jpg',  
   ],
 };
 
